0da8c50c55c16da5b167bde646794f0130a1582c
surpreet.mundra <surpreet.mundra@gmail.com>
2019-07-10 16:05:06 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -35,7 +35,7 @@ class FiguresController < ApplicationController
 
   patch '/figures/:id' do
     @figure = Figure.find(params[:id])
-    @figure.name = params[figure][name]
+    @figure.name = params["figure"]["name"]
   end
 
 
