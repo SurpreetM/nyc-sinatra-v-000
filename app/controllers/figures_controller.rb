65722ce8d60f38f5579ce94c25c18c4c8c057ab4
surpreet.mundra <surpreet.mundra@gmail.com>
2019-07-10 15:22:45 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,10 +9,14 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    @figure = Figure.create(params[:figure])
+    @figure = Figure.create(params["figure"])
 
     unless params["landmark"]["name"].empty?
       @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
     end
 
     @figure.save
